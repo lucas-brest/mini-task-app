69a11a21e31845ff274a0d9e075c70bb9877e1ea
Lucas Brest <lucasezequiel237@gmail.com>
2023-08-04 13:59:26 -0300

    Add dataannotations

--- backend/Model/Grupo.cs
+++ backend/Model/Grupo.cs
@@ -1,7 +1,10 @@
-﻿namespace backend.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model
 {
     public class Grupo
     {
+        [Key]
         public int Id { get; set; }
         public List<Usuario> Miembros { get; set; }
         public List<Tarea> Tareas { get; set; }
--- backend/Model/Tarea.cs
+++ backend/Model/Tarea.cs
@@ -1,9 +1,14 @@
-﻿namespace backend.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Model
 {
     public class Tarea
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Description { get; set; }
         public List<Usuario> Asignados { get; set; }
     }
