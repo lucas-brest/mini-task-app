6fb5bf6c0bae05db201997e2a351b8dbfe63ea9e
Lucas Brest <lucasezequiel237@gmail.com>
2023-08-04 15:15:17 -0300

    Add service, dbcontext en program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,7 @@
+using backend.Model;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddTransient<TaskService>();
+
+builder.Services.AddDbContext<TaskDBContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnectionString")));
+
 
 var app = builder.Build();
 
